37c97b196c0ba710acedcc03a16c3b2b0e1b6c18
alienz <limingxia07@gmail.com>
2019-10-09 16:11:24 +1100

    add the file

---
+++ src/layouts/BlogListView.js
@@ -0,0 +1,38 @@
+import React from "react"
+import Layout from "./layout"
+import SEO from "../components/seo"
+import { Container, Divider, Typography } from "@material-ui/core"
+import useStyles from "../styles/style"
+import BlogCard from "../components/BlogCard"
+
+const BlogListView = ({posts}) => {
+  let url = window.location.pathname;
+  if (url[url.length-1] === `/`) {
+    url = url.slice(0, url.length-1);
+  }
+  if (url[0] === `/`) {
+    url = url.slice(1, url.length);
+  }
+  const subPath = url.split('/')[0];
+  const endpoint = url.substr(url.lastIndexOf('/') + 1);
+  return (
+    <Layout>
+      <SEO title={`${subPath.charAt(0).toUpperCase() + subPath.slice(1)}: 
+      ${endpoint.toUpperCase()}`}/>
+      <Container container spacing={5} className={useStyles().container}>
+        <Typography variant="h6" gutterBottom>
+          {posts.length} {`Articles found from
+            ${subPath.charAt(0).toUpperCase() + subPath.slice(1)}:
+            ${endpoint.toUpperCase()}`}
+        </Typography>
+        {posts.map((post,index) => (
+          <div>
+            <BlogCard post={post}  key={index}/>
+            {index!==(posts.length-1) ? <Divider/> : null}
+          </div>))}
+      </Container>
+    </Layout>
+  )
+}
+
+export default BlogListView
